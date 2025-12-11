4c3fa4d0fe8cbb3f5e87ec2ca56e1e3fcc77e6fe
Daniel Andrade <dan.andrade313@gmail.com>
2025-12-11 10:51:43 -0300

    Criado projeto e realizado configurações inciais

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [
+    // Deixa disponivel em todo o projeto as variaveis de hambiente.
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // configurar acesso ao banco de dados
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 3306),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
